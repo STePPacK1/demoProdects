430cff4ba4aa37f288ee4475534917e325761a60
ST <steppack01@gmail.com>
2021-03-16 03:24:19 +0300

    down1

---
+++ KyrsachApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KyrsachApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b_back_Click(object sender, RoutedEventArgs e)
+        {
+            if (f_mainFrame.CanGoBack)
+            {
+                f_mainFrame.GoBack();
+            }
+            else
+            {
+                f_mainFrame.IsEnabled = false;
+            }
+        }
+    }
+}
